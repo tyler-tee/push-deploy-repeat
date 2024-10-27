aff6726f04128cbc9dd1fb87fd7617c0735ca947
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2024-10-27 16:15:36 -0400

    Refactor verify_signature function in update_server.py

--- server/update_server.py
+++ server/update_server.py
@@ -83,13 +83,12 @@ def verify_signature(payload_body, secret_token, signature_header):
         bool: True if the signature is valid, False otherwise.
     """
     if not signature_header:
-        # raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
         return False
+
     hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
     expected_signature = "sha256=" + hash_object.hexdigest()
 
     if not hmac.compare_digest(expected_signature, signature_header):
-        # raise HTTPException(status_code=403, detail="Request signatures didn't match!")
         return False
 
     return True
